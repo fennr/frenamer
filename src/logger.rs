ac951df58456e46ecd493931122a12d7c511da1e
fennr <fenrir1121@gmail.com>
2025-02-02 16:10:12 +0500

    fix(logger): Translate texts

--- src/logger.rs
+++ src/logger.rs
@@ -7,19 +7,19 @@ pub enum Logger {
 impl Logger {
     pub fn log_rename(self, from: &str, to: &str) {
         if let Self::Verbose = self {
-            println!("Переименование: {} -> {}", from, to);
+            println!("Renaming: {} -> {}", from, to);
         }
     }
 
     pub fn log_skip(self, file: &str) {
         if let Self::Verbose = self {
-            println!("Пропуск файла: {} (не требует изменений)", file);
+            println!("Skipping file: {} (no changes needed)", file);
         }
     }
 
     pub fn log_directory(self, dir: &str) {
         if let Self::Verbose = self {
-            println!("Обработка директории: {}", dir);
+            println!("Processing directory: {}", dir);
         }
     }
 }
